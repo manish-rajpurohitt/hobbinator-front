225946549af80093c0c98bed91aea78039431449
Manish Purohit <manishkumar9866@gmail.com>
2021-12-20 15:05:38 +0530

    Update api.js

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
  const api = axios.create({
-    baseURL : 'http://hobbinator-bakend.herokuapp.com'
+    baseURL : 'https://hobbinator-bakend.herokuapp.com'
 })
 
 export default api;
